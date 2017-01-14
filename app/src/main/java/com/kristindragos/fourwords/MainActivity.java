15eee26558b48e512cc9d6a33c95f9381678ce1b
Kristin <kristindragos@gmail.com>
2017-01-13 22:49:37 -0800

    Initial commit

---
+++ app/src/main/java/com/kristindragos/fourwords/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kristindragos.fourwords;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Log.d(TAG, "Starting game.");
+        Intent intent = new Intent(this, FindGameActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void viewHistory(View view) {
+        Log.d(TAG, "Viewing History");
+
+    }
+
+}
